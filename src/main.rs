7c79d7d4c402bdb16088a3ad5eb50dd439d035c1
Wagner Anthony <59626222+zadhart@users.noreply.github.com>
2022-07-11 10:01:23 -0300

    Spotify API is now working

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use actix_web::{post, get, web, Responder, Result, HttpResponse, App, HttpServer};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use reqwest;
+use reqwest::{self, header::{AUTHORIZATION, CONTENT_TYPE, ACCEPT}};
 
 
 #[derive(Deserialize)]
@@ -12,22 +12,35 @@ struct Info {
 #[derive(Serialize)]
 struct Test{
     nome: String,
-    bosta: String,
 } 
 
 
 /// extract `Info` using serde
 async fn index(info: web::Json<Info>) -> Result<impl Responder> {
 
-    let result = reqwest::get("https://api.spotify.com/v1/search")
-    .await
-    .unwrap()
-    .text()
-    .await;
+    let url = format!(
+        "https://api.spotify.com/v1/search?q={query}&type=track,artist",
+        // go check out her latest album. It's 🔥
+        query = "Little Simz"
+    );
+
+    let client = reqwest::Client::new();
+    let response = client
+        .get(url)
+        // confirm the request using send()
+        .header(AUTHORIZATION, "Bearer BQA9BfYoX_yElgUrqI1V3RU404_z2FC-Fbf7J7eBD6GUVEvGRRaDYg3UWkoP6JaNaceydh60v0ZFirm7GTC0HOI5i-sVKOmLeH1s0p1bcutTuQmv6023scqYvzrd7jJ9lkq2vxfvYSODjpQZtB7dEfTAJPPFvxK8wFul")
+        .header(CONTENT_TYPE, "application/json")
+        .header(ACCEPT, "application/json")
+        .send()
+        .await
+        // the rest is the same!
+        .unwrap()
+        .text()
+        .await;
+    println!("{:?}", response);
 
     let obj = Test{
         nome: info.username.to_string(),
-        bosta: result.to_string(),
     };
     
     Ok(web::Json(obj))
